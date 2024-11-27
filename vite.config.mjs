8adb8d4bbadc418279575e90d300291e8ef6f500
jhetzer <jonathan.hetzer95@gmail.com>
2024-11-27 22:11:26 +0100

    fix: Sass @import rules are deprecated

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,6 +7,13 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler' // or "modern"
+            }
+        }
+    },
     optimizeDeps: {
         noDiscovery: true
     },
